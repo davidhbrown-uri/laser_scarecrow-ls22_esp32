17b098bd93ac8bb98c09b6ddedc703af9cf3edbf
David H. Brown <dave@davidhbrown.us>
2021-12-27 23:17:25 -0500

    Create ls_event struct with type and value

--- main/events.h
+++ main/events.h
@@ -1,5 +1,9 @@
 #pragma once
-enum LSEvent {
+enum ls_event_types {
     // fire when the magnet on the rotating arm enter/leave the detection area of the Hall-effect sensor
-    Magnet_Enter, Magnet_Leave 
+    LSEVT_MAGNET_ENTER, LSEVT_MAGNET_LEAVE // <! no value for magnet events
+};
+typedef struct ls_event {
+    ls_event_types type;
+    void *value;    
 };
