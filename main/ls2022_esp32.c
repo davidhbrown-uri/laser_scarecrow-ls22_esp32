aac58b1dae947e30b9c1881e728e882cc321b9b4
David H. Brown <dave@davidhbrown.us>
2022-01-03 02:16:04 -0500

    knobs work okay, but...
    If the knob on MTDI (connector pin 4, GPIO12, ADC2 Channel 5)
    is pulled high, the flash memory won't be read correctly and
    the ESP32 won't boot until it is low.

--- main/ls2022_esp32.c
+++ main/ls2022_esp32.c
@@ -19,6 +19,7 @@
 QueueHandle_t ls_event_queue = NULL;
 
 SemaphoreHandle_t adc1_mux = NULL;
+SemaphoreHandle_t adc2_mux = NULL;
 SemaphoreHandle_t i2c_mux = NULL;
 SemaphoreHandle_t print_mux = NULL;
 
@@ -107,8 +108,8 @@ void i2c_read_temp_task(void *pvParameter)
 
 /**
  * @brief White bucket reads <<500 (usually closer to 200) and black tape reads >>2000 (usually closer to 3000)
- * 
- * @param pvParameter 
+ *
+ * @param pvParameter
  */
 void adc_read_reflectance_sensor_task(void *pvParameter)
 {
@@ -238,21 +239,19 @@ static void print_char_val_type(esp_adc_cal_value_t val_type)
     }
 }
 
-
-volatile uint32_t stepperstep_level = 0;  
+volatile uint32_t stepperstep_level = 0;
 static bool IRAM_ATTR stepper_step_isr_callback(void *args)
 {
     BaseType_t high_task_awoken = pdFALSE;
-    gpio_set_level(LSGPIO_STEPPERSTEP, stepperstep_level++%2);
-   /* See timer_group_example for how to use this: */
-//    xQueueSendFromISR(s_timer_queue, &evt, &high_task_awoken);
+    gpio_set_level(LSGPIO_STEPPERSTEP, stepperstep_level++ % 2);
+    /* See timer_group_example for how to use this: */
+    //    xQueueSendFromISR(s_timer_queue, &evt, &high_task_awoken);
 
     return high_task_awoken == pdTRUE; // return whether we need to yield at the end of ISR
-
 }
 static void stepper_task(void *pvParameter)
 {
-    gpio_set_level(LSGPIO_STEPPERSLEEP, 0);//don't do anything while we get ready
+    gpio_set_level(LSGPIO_STEPPERSLEEP, 0); // don't do anything while we get ready
     // see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/timer.html
     // defaut ESP32 clock source is 80MHz (1MHz rate = 1μs period)
     // Stepper is set to 1/16 microsteps, so 200*16=3200 steps per rotation
@@ -265,7 +264,7 @@ static void stepper_task(void *pvParameter)
     //  - 50,000 for the fastest step
     //  - 5,500,000 for the slowest step
     // The timers are 64-bit, so counting this number of steps should not be an issue
-    // Tather than aiming for 1ms pulses, toggling at the total timer count for 
+    // Tather than aiming for 1ms pulses, toggling at the total timer count for
     // a square(ish) wave would make sense.
     timer_config_t stepper_step_timer_config = {
         .divider = STEPPER_TIMER_DIVIDER,
@@ -276,7 +275,7 @@ static void stepper_task(void *pvParameter)
     };
     ESP_ERROR_CHECK(timer_init(TIMER_GROUP_0, TIMER_0, &stepper_step_timer_config));
     ESP_ERROR_CHECK(timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0ULL));
-    ESP_ERROR_CHECK(timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, APB_CLK_FREQ / STEPPER_TIMER_DIVIDER / 1500 ));
+    ESP_ERROR_CHECK(timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, APB_CLK_FREQ / STEPPER_TIMER_DIVIDER / 1500));
     ESP_ERROR_CHECK(timer_enable_intr(TIMER_GROUP_0, TIMER_0));
     ESP_ERROR_CHECK(timer_isr_callback_add(TIMER_GROUP_0, TIMER_0, stepper_step_isr_callback, NULL, 0));
     ESP_ERROR_CHECK(timer_start(TIMER_GROUP_0, TIMER_0));
@@ -284,7 +283,7 @@ static void stepper_task(void *pvParameter)
     {
         gpio_set_level(LSGPIO_STEPPERDIRECTION, 0);
         gpio_set_level(LSGPIO_STEPPERSLEEP, 1);
-        //should delay 1ms here before stepping, but deal with that in production code
+        // should delay 1ms here before stepping, but deal with that in production code
         xSemaphoreTake(print_mux, portMAX_DELAY);
         printf("Stepper forward (%d)\n", stepperstep_level);
         xSemaphoreGive(print_mux);
@@ -302,6 +301,36 @@ static void stepper_task(void *pvParameter)
     }
 }
 
+void adc2_read_knobs_task(void *pvParameter)
+{
+#define KNOB_COUNT 4
+    adc2_channel_t knob_channels[] = {LSADC2_KNOB3, LSADC2_KNOB4, LSADC2_KNOB5, LSADC2_KNOB6};
+    uint32_t knob_readings[KNOB_COUNT];
+    while (1)
+    {
+        for (int knob = 0; knob < KNOB_COUNT; knob++)
+        {
+            xSemaphoreTake(adc2_mux, pdMS_TO_TICKS(1000));
+            int adc_reading = 0;
+            uint32_t adc_sum = 0;
+            // atten 11 by default... shouldn't need to focus on lower voltages?
+            ESP_ERROR_CHECK(adc2_config_channel_atten(knob_channels[knob], ADC_ATTEN_11db));
+            for (int i = 0; i < 4; i++)
+            {
+                ESP_ERROR_CHECK(adc2_get_raw(knob_channels[knob], ADC_WIDTH_12Bit, &adc_reading));
+                adc_sum += adc_reading;
+            }
+            knob_readings[knob] = adc_sum / 4;
+            xSemaphoreGive(adc2_mux);
+            vTaskDelay(2); // yield some time to other tasks... we're in no particular hurry
+        }
+        xSemaphoreTake(print_mux, portMAX_DELAY);
+        printf("Knobs: %d\t %d\t %d\t %d\n", knob_readings[0], knob_readings[1], knob_readings[2], knob_readings[3]);
+        xSemaphoreGive(print_mux);
+        vTaskDelay(pdMS_TO_TICKS(2500));
+    }
+}
+
 void app_main(void)
 {
     ls_event_queue = xQueueCreate(32, sizeof(enum ls_event_types));
@@ -314,6 +343,7 @@ void app_main(void)
     printf("Checked ADC efuse\n");
     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     adc1_mux = xSemaphoreCreateMutex();
+    adc2_mux = xSemaphoreCreateMutex();
     i2c_mux = xSemaphoreCreateMutex();
     print_mux = xSemaphoreCreateMutex();
     printf("Initializing GPIO\n");
@@ -343,5 +373,6 @@ void app_main(void)
     printf("Started magnet sense ISR\n");
     xTaskCreate(&adc_read_reflectance_sensor_task, "reflectance", configMINIMAL_STACK_SIZE * 3, NULL, 1, NULL);
     xTaskCreate(&stepper_task, "stepper", configMINIMAL_STACK_SIZE * 3, NULL, 1, NULL);
+    xTaskCreate(&adc2_read_knobs_task, "knobs", configMINIMAL_STACK_SIZE * 3, NULL, 1, NULL);
     printf("Started all test tasks\n");
 }
