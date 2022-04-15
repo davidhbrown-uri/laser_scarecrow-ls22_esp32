7a39a9191df7dec6589b50b0af7b5191475b5ec7
David H. Brown <dave@davidhbrown.us>
2022-04-14 20:22:47 -0400

    Lowercased ls_map_*_bit definitions
    to look like functions... no reason to make the devs remember these are
    actually #define not real functions

--- main/map.c
+++ main/map.c
@@ -2,27 +2,27 @@
 #include "tape.h"
 #include "config.h"
 
-static bool _ls_map_ignore = false;
+static bool IRAM_ATTR _ls_map_ignore = false;
 
 #define LS_MAP_BYTES_REQUIRED (LS_STEPPER_STEPS_PER_ROTATION / 8 / LS_MAP_RESOLUTION)
-static uint8_t _ls_map_data[LS_MAP_BYTES_REQUIRED];
+static uint8_t IRAM_ATTR _ls_map_data[LS_MAP_BYTES_REQUIRED];
 // http://www.mathcs.emory.edu/~cheung/Courses/255/Syllabus/1-C-intro/bit-array.html
-#define LS_MAP_SET_BIT(map_index) ( _ls_map_data[(map_index / 8)] |= (1 << (map_index % 8)))
-#define LS_MAP_CLEAR_BIT(map_index) ( _ls_map_data[(map_index / 8)] &= ~(1 << (map_index % 8)))
-#define LS_MAP_READ_BIT(map_index)    ( _ls_map_data[(map_index / 8)] & (1 << (map_index % 8)) )
+#define ls_map_set_bit(map_index) ( _ls_map_data[(map_index / 8)] |= (1 << (map_index % 8)))
+#define ls_map_clear_bit(map_index) ( _ls_map_data[(map_index / 8)] &= ~(1 << (map_index % 8)))
+#define ls_map_read_bit(map_index)    ( _ls_map_data[(map_index / 8)] & (1 << (map_index % 8)) )
 
 
 void ls_map_enable_at(int32_t stepper_position)
 {
-    LS_MAP_SET_BIT(stepper_position / LS_MAP_RESOLUTION);
+    ls_map_set_bit(stepper_position / LS_MAP_RESOLUTION);
 }
 void ls_map_disable_at(int32_t stepper_position)
 {
-    LS_MAP_CLEAR_BIT(stepper_position / LS_MAP_RESOLUTION);
+    ls_map_clear_bit(stepper_position / LS_MAP_RESOLUTION);
 }
-bool ls_map_is_enabled_at(int32_t stepper_position)
+bool IRAM_ATTR ls_map_is_enabled_at(int32_t stepper_position)
 {
-    return _ls_map_ignore || (LS_MAP_READ_BIT(stepper_position / LS_MAP_RESOLUTION)) > 0;
+    return _ls_map_ignore || (ls_map_read_bit(stepper_position / LS_MAP_RESOLUTION)) > 0;
 }
 void ls_map_ignore(void)
 {
