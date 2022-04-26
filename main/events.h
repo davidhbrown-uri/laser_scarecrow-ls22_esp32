802f6447ae98e1c9034e6b42062a24292ca7094a
David H. Brown <dave@davidhbrown.us>
2022-04-26 10:47:03 -0400

    commented event list

--- main/events.h
+++ main/events.h
@@ -3,40 +3,40 @@
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
 
-enum ls_event_types
+enum ls_event_t
 {
-    LSEVT_NOOP=0, // 0: nothing happened, but we need an event
-    LSEVT_STATE_ENTRY=1, // 1: allow successor state to initialize
-    // fire when the magnet on the rotating arm enter/leave the detection area of the Hall-effect sensor
-    LSEVT_MAGNET_ENTER = 20, 
-    LSEVT_MAGNET_LEAVE, 
+    LSEVT_NOOP=0, // nothing happened, but we need an event to keep processing internal behavior
+    LSEVT_STATE_ENTRY=1, // inserted by state machine on state change to allow new state to initialize
+    
+    LSEVT_MAGNET_ENTER = 20, // rotating arm's magnet enters detection area
+    LSEVT_MAGNET_LEAVE, // rotating arm's magnet leaves detection area
 
-    LSEVT_STEPPER_FINISHED_MOVE = 30, 
+    LSEVT_STEPPER_FINISHED_MOVE = 30, // rotating arm finishes movement
 
-    LSEVT_SERVO_FINISHED_MOVE = 40,
+    LSEVT_SERVO_FINISHED_MOVE = 40, // servo tilt reaches target position
 
-    LSEVT_LIGHT_DAY = 50,
-    LSEVT_LIGHT_NIGHT,
+    LSEVT_LIGHT_DAY = 50, // ambient light level moves above daytime light level threshold 
+    LSEVT_LIGHT_NIGHT, // ambient light level moves below nighttime light level threshold 
 
-    LSEVT_CONTROLS_CONNECTED = 60,
-    LSEVT_CONTROLS_DISCONNECTED, 
-    LSEVT_CONTROLS_SPEED,
-    LSEVT_CONTROLS_TOPANGLE,
-    LSEVT_CONTROLS_BOTTOMANGLE,
+    LSEVT_CONTROLS_CONNECTED = 60, // external controls switched "on"
+    LSEVT_CONTROLS_DISCONNECTED, // extenral controls switched "off"
+    LSEVT_CONTROLS_SPEED, // external control slider 1 moved (ls_event.value is pointer to ADC value)
+    LSEVT_CONTROLS_TOPANGLE, // external control slider 2 moved (ls_event.value is pointer to ADC value)
+    LSEVT_CONTROLS_BOTTOMANGLE, // external control slider 3 moved (ls_event.value is pointer to ADC value)
 
-    LSEVT_BUZZER_WARNING_COMPLETE = 80,
+    LSEVT_BUZZER_WARNING_COMPLETE = 80, // long pre-laser warning sequence of tones has finished
 
-    LSEVT_HOME_COMPLETED = 100,
-    LSEVT_HOME_FAILED,
-    LSEVT_REHOME_REQUIRED,
+    LSEVT_HOME_COMPLETED = 100, // the homing routine has moved the arm to the reference location
+    LSEVT_HOME_FAILED, // the homing routing could not locate the reference location
+    LSEVT_REHOME_REQUIRED, // homing must be done again to ensure stability of position over time
 
-    LSEVT_MAP_COMPLETED = 110,
-    LSEVT_MAP_FAILED 
-} ls_event_types;
+    LSEVT_MAP_COMPLETED = 110, // the locations where the laser can't be on has successfully been read
+    LSEVT_MAP_FAILED // the locations where the laser can't be on could not be read
+}; 
 
 typedef struct ls_event
 {
-    enum ls_event_types type;
+    enum ls_event_t type;
     void *value;
 } ls_event;
 
