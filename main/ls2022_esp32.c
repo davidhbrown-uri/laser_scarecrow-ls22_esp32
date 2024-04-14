59793bd4e1eb581f6514254581e3e42e2f42cd4f
David Brown <dave@davidhbrown.us>
2024-04-14 11:03:05 -0400

    Limit servo rate to help avoid premature failure

--- main/ls2022_esp32.c
+++ main/ls2022_esp32.c
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#define VERSION_MESSAGE "URI Laser Scarecrow 2023 version 2.0.0 (Apr 24, 2023)\n"
+#define VERSION_MESSAGE "URI Laser Scarecrow 2023 version 2.0.1 (Apr 14, 2024)\n"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
